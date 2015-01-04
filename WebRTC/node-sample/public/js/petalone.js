9c36443d80f07ae7a1e1b5032fdec7471addc408
Robert Mao <zhihong.mao@gmail.com>
2015-01-04 09:28:26 -0800

    adding buttons for owner

--- WebRTC/node-sample/public/js/petalone.js
+++ WebRTC/node-sample/public/js/petalone.js
@@ -2,4 +2,12 @@ $(function() {
   $('body').on("click", "#treat", function() {
     alert("send a treat!");
   });
+  $('body').on("click", "#calldog", function() {
+
+    dialCall('14254425101', 'video',  document.getElementById('localVideo'), document.getElementById('remoteVideo'));
+
+  });
+  $('body').on("click", "#whistle", function() {
+    alert("Kaman API for playing Whistle!");
+  });
 });
